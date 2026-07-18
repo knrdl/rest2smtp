f544dcfb042b668fb002af8a4b73e785baa6c043
knrdl <35548889+knrdl@users.noreply.github.com>
2026-07-18 13:15:26 +0000

    style: fmt

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,19 +1,25 @@
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{AsyncSmtpTransport, Tokio1Executor};
 
-use super::config::{SmtpEncryption, SmtpConfig};
+use super::config::{SmtpConfig, SmtpEncryption};
 
 pub struct Mailer {
-    pub transport: AsyncSmtpTransport::<Tokio1Executor>,
+    pub transport: AsyncSmtpTransport<Tokio1Executor>,
     pub config: SmtpConfig,
 }
 
 impl Mailer {
     pub fn new(config: crate::config::SmtpConfig) -> Mailer {
         let mut sender = match config.encryption {
-            SmtpEncryption::Tls => AsyncSmtpTransport::<Tokio1Executor>::relay(&config.host).unwrap(),
-            SmtpEncryption::StartTls => AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&config.host).unwrap(),
-            SmtpEncryption::Unencrypted => AsyncSmtpTransport::<Tokio1Executor>::builder_dangerous(&config.host),
+            SmtpEncryption::Tls => {
+                AsyncSmtpTransport::<Tokio1Executor>::relay(&config.host).unwrap()
+            }
+            SmtpEncryption::StartTls => {
+                AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&config.host).unwrap()
+            }
+            SmtpEncryption::Unencrypted => {
+                AsyncSmtpTransport::<Tokio1Executor>::builder_dangerous(&config.host)
+            }
         };
         if let (Some(u), Some(p)) = (&config.username, &config.password) {
             sender = sender.credentials(Credentials::new(u.to_string(), p.to_string()));
@@ -21,7 +27,9 @@ impl Mailer {
         if let Some(port) = &config.port {
             sender = sender.port(*port)
         }
-        Mailer { transport: sender.build(), config }
+        Mailer {
+            transport: sender.build(),
+            config,
+        }
     }
 }
-
